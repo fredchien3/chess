85d11c4178574ff03752cb0a6b2ba38c27ee995f
Frederick <fred.chien3@gmail.com>
2022-01-26 16:28:44 +0800

    initialize nullpiece

---
+++ nullpiece.rb
@@ -0,0 +1,10 @@
+require_relative 'piece.rb'
+require 'singleton'
+
+class Nullpiece < Piece
+    include Singleton
+    attr_reader :color, :symbol
+    def initialize
+        @symbol = :_
+    end
+end
